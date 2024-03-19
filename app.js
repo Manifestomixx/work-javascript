ccfc18a9dea27ddee5dcc5b6c28c4c27cd23caa7
manifesto <manifestomixx@gmail.com>
2024-03-19 16:02:35 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+// let headingTag = document.getElementsByTagName("h1");
+//  console.log(headingTag);
+// headingTag.color="red";
+// let newHeading = [...headingTag]
+// newHeading.forEach((singleTag)=> singleTag.style.color ="red");
+
+// document.body.style.backgroundColor = "yellow";
+
+// innerHTML
+// let newLink = document.querySelector("h1");
+// newLink.innerHTML = "<h1> Obinna </h1>";
+
+// let dec = document.querySelector('.dec');
+// let h1 = document.querySelector('h1');
+// let inc = document.querySelector('.inc')
+
+// dec.addEventListener('click',function(){
+//     h1.innerText = "Steff"
+// })
+
+
+// // line 6
+// function bodyChanger(){
+//     document.body.style.backgroundColor = "red"
+// }
+
+// inc.addEventListener('click',bodyChanger)
+
+// Decrease
+let dec = document.querySelector(".dec");
+let h1 = document.querySelector("h1");
+
+function decremental() {
+    h1.innerText --;
+    document.body.style.backgroundColor = "red";
+    
+}
+dec.addEventListener("click",decremental)
+
+
+// increase
+let inc = document.querySelector(".inc");
+let newH1 = document.querySelector("h1");
+function incremental(){
+    newH1.innerText ++;
+    document.body.style.backgroundColor = "green";
+    
+    
+}
+
+
+inc.addEventListener("click",incremental)
+
+// reset
+let res = document.querySelector(".res");
+let nH1 = document.querySelector("h1");
+function reset(){
+    nH1.innerText  = [0];
+    document.body.style.backgroundColor = "yellow";
+}
+
+
+res.addEventListener("click",reset)
+
+
+// Take home
